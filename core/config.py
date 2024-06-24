a1d9e98ab085ecb2c70781d5f92ecd1c9cd3a170
Toan Phan <thisismisterbrian@gmail.com>
2024-06-24 12:35:46 +0700

    cd: enhance

--- core/config.py
+++ core/config.py
@@ -2,21 +2,21 @@ import os
 from dotenv import find_dotenv, load_dotenv
 from pydantic_settings import BaseSettings
 
-ENV = os.getenv("ENV", "")
+# ENV = os.getenv("ENV", "")
 
-def load_dotenv_fallback(env_files):
-    for env_file in env_files:
-        dotenv_path = find_dotenv(env_file)
-        if dotenv_path:
-            load_dotenv(dotenv_path)
-            return dotenv_path
-    return None
+# def load_dotenv_fallback(env_files):
+#     for env_file in env_files:
+#         dotenv_path = find_dotenv(env_file)
+#         if dotenv_path:
+#             load_dotenv(dotenv_path)
+#             return dotenv_path
+#     return None
 
-ENV_FILES = {
-    "env": [".env.production", ".env.development", ".env"],
-}
+# ENV_FILES = {
+#     "env": [".env.production", ".env.development", ".env"],
+# }
 
-dotenv_path = load_dotenv_fallback(ENV_FILES.get(ENV, ENV_FILES["env"]))
+# dotenv_path = load_dotenv_fallback(ENV_FILES.get(ENV, ENV_FILES["env"]))
 class Settings(BaseSettings):
     OPENAI_API_KEY: str
     GEMINI_API_KEY: str
@@ -28,6 +28,6 @@ class Settings(BaseSettings):
     ALLOWED_HOST_2: str
     PORT: int
     class Config:
-       env_file = dotenv_path
+       env_file = '.env'
 
 settings = Settings()
