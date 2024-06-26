f194737eca795a0c74293344df1b21cc0a0c1f94
Toan Phan <thisismisterbrian@gmail.com>
2024-06-27 00:13:32 +0700

    feat: ratelimit

--- gateways/ai.py
+++ gateways/ai.py
@@ -1,16 +1,14 @@
 from fastapi import APIRouter, Depends, WebSocket, WebSocketDisconnect
-from constants.ai import CONTEXT_EXPIRE_TIME
-from models.context import Context
-from models.message import Message
+from constants.ai import AI_WS_ACTION_TYPE
 from services.ai import (
     generate_initial_conversation,
-    query_gemini,
-    query_openai,
-    # send_by_token,
+    handle_beginning_conversion,
+    handle_rate_limit,
+    handle_send_message,
+    handle_switch_model,
 )
 from utils.websocket import ConnectionManager
 from redis.asyncio import Redis
-import uuid
 
 router = APIRouter()
 manager = ConnectionManager()
@@ -23,76 +21,62 @@ async def websocket_endpoint(
     device_id: str,
     redis: Redis = Depends(manager.get_redis),
 ):
-
     await manager.connect(websocket, device_id)
     context_key = f"context:{device_id}"
     context_data = await redis.get(context_key)
 
     if context_data:
-        context = Context.parse_raw(context_data)
-        if len(context.messages) == 1:
-            await generate_initial_conversation(
-                websocket=websocket, locale=locale, device_id=device_id, redis=redis
-            )
-        else:
-            await websocket.send_json(
-                {
-                    "type": "context",
-                    "data": {
-                        "messages": [message.dict() for message in context.messages]
-                    },
-                }
-            )
+        await handle_beginning_conversion(
+            websocket=websocket,
+            locale=locale,
+            context_key=context_key,
+            context_data=context_data,
+            redis=redis,
+        )
     else:
         await generate_initial_conversation(
-            websocket=websocket, locale=locale, device_id=device_id, redis=redis
+            websocket=websocket, locale=locale, context_key=context_key, redis=redis
         )
 
     try:
         while True:
 
+            await handle_rate_limit(
+                websocket=websocket,
+                locale=locale,
+                device_id=device_id,
+                context_key=context_key,
+                context_data=context_data,
+                redis=redis,
+            )
+
             data = await websocket.receive_json()
-            action = data.get("action", "send_message")
+            action = data.get("action", AI_WS_ACTION_TYPE.SEND_MESSAGE)
 
-            if action == "send_message":
-                model = data.get("model", "ChatGPT")
-                prompt = data.get("prompt", "").strip()
-
-                context_data = await redis.get(context_key)
-                context = Context.parse_raw(context_data)
-
-                message = Message(id=str(uuid.uuid4()), prompt=f"You: {prompt}")
-                context.messages.append(message)
-                await websocket.send_json(
-                    {"type": "client_message", "data": message.dict()}
-                )
-                await redis.set(context_key, context.json(), ex=CONTEXT_EXPIRE_TIME)
-
-                try:
-                    if model == "ChatGPT":
-                        response = await query_openai(prompt)
-                    else:
-                        response = await query_gemini(prompt)
-                except Exception:
-                    response = (
-                        "Mẫu này hiện không hỗ trợ, vui lòng chọn mẫu khác."
-                        if locale == "vi"
-                        else "This model is currently unavailable, please select the other one."
-                    )
-
-                ai_message = Message(id=str(uuid.uuid4()), prompt=f"AI: {response}")
-                context.messages.append(ai_message)
-                await redis.set(context_key, context.json(), ex=CONTEXT_EXPIRE_TIME)
-
-                # words = response.split()
-                # await send_by_token(websocket, ai_message.id, words)
-                await websocket.send_json(
-                    {"type": "ai_message", "data": ai_message.dict()}
+            if action == AI_WS_ACTION_TYPE.SEND_MESSAGE:
+                await handle_send_message(
+                    websocket=websocket,
+                    locale=locale,
+                    context_key=context_key,
+                    data=data,
+                    redis=redis,
                 )
 
-            elif action == "new_context":
+            elif action == AI_WS_ACTION_TYPE.SWITCH_MODEL:
+                await handle_switch_model(
+                    websocket=websocket,
+                    locale=locale,
+                    context_key=context_key,
+                    data=data,
+                    redis=redis,
+                )
+
+            elif action == AI_WS_ACTION_TYPE.NEW_CONTEXT:
                 await generate_initial_conversation(
-                    websocket=websocket, locale=locale, device_id=device_id, redis=redis
+                    websocket=websocket,
+                    locale=locale,
+                    context_key=context_key,
+                    redis=redis,
                 )
 
     except WebSocketDisconnect:
